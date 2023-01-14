45f3d2ec2f2d43efe780cccb80fd1a13fd016377
ku70t6h1k6r1 <uju5150@gmail.com>
2023-01-14 12:07:53 +0900

    formatter_js 更新

--- static/js/input-field/formatter.js
+++ static/js/input-field/formatter.js
@@ -4,36 +4,54 @@ import {
 } from './grid.js';
 
 export const score2json=(score)=>{
-    console.log(score[0].length);
-    var score_json = Array(score[0].length); //配列が空だったらエラーが出る。
+    //validation
+    if(score.length != pitch_range){
+        return false;
+    }
+
+
+    var score_json = Array(score[0].length); 
     score_json.fill(null);
 
     var note_id_past = null;
-    for(let pitch=0; pitch < score.length; pitch++ ){
-        for(let beat=0; beat < score[pitch].length; beat++){
-            if(score_json[beat] === null){
-                if(score[pitch][beat]){
-                    if(note_id_past==score[pitch][beat]){
-                        score_json[beat] = -1;
-                    }else{
-                        score_json[beat] = pitch2midinote(pitch);
-                    }
-                    
-                    console.log(pitch2midinote(pitch));
-                    note_id_past = score[pitch][beat];
-                }else{
-                    if(note_id_past==-2){
-                        score_json[beat] = -1;
-                    }else{
-                        score_json[beat] = -2;
-                    }               
-                    note_id_past = -2
-                }    
+
+    for(let beat=0; beat < score[0].length; beat++){
+        //
+        var pitch_onbeat = null;
+
+        //そのビートのピッチを抽出
+        var note_id_current = null
+        for(let pitch=0; pitch < pitch_range; pitch++ ){
+            if(score[pitch][beat]){
+                note_id_current = score[pitch][beat];
+                pitch_onbeat = pitch;
             }
+        }
+
+        //前のビートと比較
+        if(note_id_current == note_id_past){
+            //同じだった時
+            score_json[beat] = -1;
+        }else{
+            //違った時
+            if(note_id_current){
+                score_json[beat] = pitch2midinote(pitch_onbeat);
+            }else{
+                score_json[beat] = -2;   
+            }
+            
+            //note_id_past更新
+            note_id_past = note_id_current
         }
     }
 
+    if(score_json[0] == -1){
+        score_json[0] = -2
+    }
     console.log(score_json);
+
+
+    
 
 }
 
